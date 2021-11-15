c1c4c96b3c64590bd18cb8cdfd3145fa5bd1fc77
LucianaVal06 <70452459+LucianaVal06@users.noreply.github.com>
2021-11-15 01:45:02 -0300

    Add files via upload

---
+++ ejObliBaseDeDatos/ejercicioObligatorio.sql
@@ -0,0 +1,29 @@
+-- CREATE DATABASE `ejcacbd`;
+-- USE ejcacbd;
+-- CREATE TABLE DatosPersona (
+--     id int(11) AUTO_INCREMENT PRIMARY KEY,
+--     nombre VARCHAR(40) NOT NULL,
+--     apellido VARCHAR(40) NOT NULL,
+--     edad TINYINT(2) NOT NULL,
+--     fecha TIMESTAMP,
+--     provincia VARCHAR(30) NOT NULL
+-- );
+
+-- SET time_zone ='+03:00';
+
+-- INSERT INTO datospersona (nombre, apellido, edad, fecha, provincia)
+-- VALUES ('Lola', 'Perez', 35, NOW(), 'Catamarca');
+
+-- INSERT INTO datospersona (nombre, apellido, edad, fecha, provincia)
+-- VALUES ('Maria', 'Lopez', 23, NOW(), 'La Rioja');
+
+-- INSERT INTO datospersona (nombre, apellido, edad, fecha, provincia)
+-- VALUES ('Rosa', 'Martinez', 58, NOW(), 'Santa Cruz');
+
+-- INSERT INTO datospersona (nombre, apellido, edad, fecha, provincia)
+-- VALUES ('Brenda', 'Ruiz', 42, NOW(), 'Jujuy');
+
+-- INSERT INTO datospersona (nombre, apellido, edad, fecha, provincia)
+-- VALUES ('Marta', 'Rodriguez', 67, NOW(), 'Misiones');
+
+-- SELECT * FROM datospersona;
